bcbe2934e949bd6a0c1a031887be6cf41a9b18e1
Ryan Cumming <etaoins@gmail.com>
2013-04-25 07:24:11 -0700

    Rework test to use OptionValues
    This lets us access our scope a bit more explicitly without nesting an
    inside block

--- compiler/src/test/scala/ExtractBodySuite.scala
+++ compiler/src/test/scala/ExtractBodySuite.scala
@@ -1,59 +1,47 @@
 package test.scala
 
-import org.scalatest.{FunSuite,Inside}
+import org.scalatest.{FunSuite,Inside,OptionValues}
 import llambda._
 
-class ExtractBodySuite extends FunSuite with Inside with ExpressionHelpers {
+class ExtractBodySuite extends FunSuite with Inside with OptionValues with ExpressionHelpers {
   implicit val primitiveScope = new Scope(SchemePrimitives.bindings)
 
   test("define variable") {
     inside(bodyFor("(define a 2)")) {
       case (exprs, scope) =>
-        inside(scope.get("a")) {
-          case Some(locA) =>
-            assert(exprs == List(
-              et.SetVar(locA, et.Literal(ast.IntegerLiteral(2)))
-            ))
-        }
+        assert(exprs == List(
+          et.SetVar(scope.get("a").value, et.Literal(ast.IntegerLiteral(2)))
+        ))
     }
   }
 
   test("redefine variable") {
     inside(bodyFor("(define a 2)(define a 3)")) {
       case (exprs, scope) =>
-        inside(scope.get("a")) {
-          case Some(locA) =>
-            assert(exprs == List(
-              et.SetVar(locA, et.Literal(ast.IntegerLiteral(2))),
-              et.SetVar(locA, et.Literal(ast.IntegerLiteral(3)))
-            ))
-        }
+        assert(exprs == List(
+          et.SetVar(scope.get("a").value, et.Literal(ast.IntegerLiteral(2))),
+          et.SetVar(scope.get("a").value, et.Literal(ast.IntegerLiteral(3)))
+        ))
     }
   }
   
   test("indirect define") {
     inside(bodyFor("(define a 2)(define b a)")) {
       case (exprs, scope) =>
-        inside((scope.get("a"), scope.get("b"))) {
-          case (Some(locA), Some(locB)) =>
-            assert(exprs == List(
-              et.SetVar(locA, et.Literal(ast.IntegerLiteral(2))),
-              et.SetVar(locB, et.VarReference(locA))
-            ))
-        }
+        assert(exprs == List(
+          et.SetVar(scope.get("a").value, et.Literal(ast.IntegerLiteral(2))),
+          et.SetVar(scope.get("b").value, et.VarReference(scope.get("a").value))
+        ))
     }
   }
   
   test("reference variable") {
     inside(bodyFor("(define a 2) a")) {
       case (exprs, scope) =>
-        inside(scope.get("a")) {
-          case Some(locA) =>
-            assert(exprs == List(
-              et.SetVar(locA, et.Literal(ast.IntegerLiteral(2))),
-              et.VarReference(locA)
-            ))
-        }
+        assert(exprs == List(
+          et.SetVar(scope.get("a").value, et.Literal(ast.IntegerLiteral(2))),
+          et.VarReference(scope.get("a").value)
+        ))
     }
   }
 }
