9a7cc6aa0331b880c0e0034cfc75bf6f9b903540
Ryan Cumming <etaoins@gmail.com>
2013-06-09 21:39:04 -0700

    Define (begin), (let), (and), (or) and (when)
    These are simple macros that can be defined purely in terms of primitive
    expressions we already support. Their definitions are taken verbatim
    from R7RS with the exception of tagged (let) which requires (letrec)

--- compiler/src/main/resources/libraries/scheme/core.scm
+++ compiler/src/main/resources/libraries/scheme/core.scm
@@ -1,3 +1,43 @@
 (define-library (scheme core)
 	(import (llambda primitives))
-	(export lambda quote if set! syntax-error))
+	(export lambda quote if set! syntax-error)
+
+	(export begin)
+	(begin
+	  (define-syntax begin
+		 (syntax-rules ()
+							((begin exp ...)
+							 ((lambda () exp ...))))))
+
+	(export let)
+	(begin
+	  ; This isn't the full definition - tagged let isn't supported
+	  (define-syntax let
+		 (syntax-rules ()
+							((let ((name val) ...) body1 body2 ...)
+							 ((lambda (name ...) body1 body2 ...)
+							  val ...)))))
+
+	(export and or when)
+	(begin
+	  (define-syntax and
+		 (syntax-rules ()
+							((and) #t)
+							((and test) test)
+							((and test1 test2 ...)
+							 (if test1 (and test2 ...) #f))))
+
+	  (define-syntax or
+		 (syntax-rules ()
+							((or) #f)
+							((or test) test)
+							((or test1 test2 ...)
+							 (let ((x test1))
+								(if x x (or test2 ...))))))
+
+	  (define-syntax when
+		 (syntax-rules ()
+							((when test result1 result2 ...)
+							 (if test
+								(begin result1 result2 ...))))))
+)
