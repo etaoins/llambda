ec294377dc9e6fad8e4ae76d4e634137cee98cc5
Ryan Cumming <etaoins@gmail.com>
2013-06-12 19:32:22 -0700

    Change nfi.Bool to nfi.Bool32
    nfi.Bool was previously defined as i1. Predicates are expressed in LLVM
    IR as i8 but the x86-64 ABI packs it in an i32. We should be using the
    i32 version when describing function signatures.
    
    At the same time add an alias from "bool" => nfi.Bool32 in case we port
    to an ABI that uses a different sized bool

--- compiler/src/main/scala/llambda/frontend/ExtractModuleBody.scala
+++ compiler/src/main/scala/llambda/frontend/ExtractModuleBody.scala
@@ -57,7 +57,7 @@ object ExtractModuleBody {
 
   private def createNativeFunction(fixedArgData : List[sst.ScopedDatum], restArgType : Option[String], returnTypeString : String, nativeSymbol : String) : et.NativeFunction = {
     def parseNativeType(typeString : String) : nfi.NativeType = typeString match {
-      case "bool"   => nfi.Bool
+      case "bool32" => nfi.Bool32
       case "int8"   => nfi.Int8
       case "int16"  => nfi.Int16
       case "int32"  => nfi.Int32
@@ -67,6 +67,7 @@ object ExtractModuleBody {
 
       // XXX: This assumes Unix-like LP64: 64bit Linux, FreeBSD, Mac OS X, etc 
       // These aliases are here so we can do the right thing when porting to other archs
+      case "bool"  => nfi.Bool32
       case "short" => nfi.Int16
       case "int"   => nfi.Int32
       case "long"  => nfi.Int64
--- compiler/src/main/scala/llambda/nfi/NativeType.scala
+++ compiler/src/main/scala/llambda/nfi/NativeType.scala
@@ -10,7 +10,7 @@ class IntType(val bits : Integer) extends NativeType {
   val schemeType : Option[st.SchemeType] = Some(st.ExactIntegerType)
 }
 
-case object Bool extends IntType(1) {
+case object Bool32 extends IntType(32) {
   override val schemeType = Some(st.BooleanType)
 }
 
