06c65c9d0ced0dafc42a3483d089fa4f7a436a3d
Ryan Cumming <etaoins@gmail.com>
2013-07-17 15:42:12 -0700

    Implement call instruction generation
    This should cover all possible call syntaxes except for:
    - Discarding the result of non-void calls
    - Any attributes that are otherwise unimplemented

--- compiler/src/main/scala/llambda/codegen/llvmir/OtherInstrs.scala
+++ compiler/src/main/scala/llambda/codegen/llvmir/OtherInstrs.scala
@@ -28,4 +28,68 @@ private[llvmir] trait OtherInstrs extends IrBuilder {
 
     resultVar
   }
+
+  protected def callDecl(decl : IrFunctionDeclLike, arguments : Seq[IrValue], tailCall : Boolean = false) : Option[LocalVariable] = {
+    call(decl, decl.irValue, arguments, tailCall)
+  }
+
+  protected def call(callable : IrCallableLike, functionPtr : IrValue, arguments : Seq[IrValue], tailCall : Boolean = false) : Option[LocalVariable] = {
+    // We only return a result for non-void result types
+    val resultVarOpt = callable.result.irType match {
+      case VoidType =>
+        None
+      case otherType : FirstClassType =>
+        Some(allocateLocalVar(otherType))
+    }
+
+    // If we're non-void we return a value
+    val assignmentIrOpt = resultVarOpt.map(_.toIr + " =")
+
+    // Tail calls are prefixed with "tail"
+    val tailCallIrOpt = if (tailCall) {
+      Some("tail")
+    }
+    else {
+      None
+    }
+
+    // Add our calling convention if we're using a non-default one
+    val callingConvIrOpt = callable.callingConv.toOptIr
+
+    // Only zeroext, signext, inreg are allowed here
+    // We don't support inreg
+    val filteredRetAttrs = callable.result.attributes.intersect(Set(IrFunction.ZeroExt, IrFunction.SignExt))
+    val retAttrIrs = filteredRetAttrs.map(_.toIr)
+
+    val resultTypeIr = callable.result.irType.toIr
+
+    val functionPtrIr = functionPtr.toIr
+
+    if (arguments.length != callable.arguments.length) {
+      throw new InternalCompilerErrorException("Passed argument count didn't match callable argument count")
+    }
+
+    // Combine the passed arguments with their attributes from their callable
+    val argIr = (arguments zip (callable.arguments)) map { case (arg, argDecl) =>
+      if (arg.irType != argDecl.irType) {
+        throw new InternalCompilerErrorException(s"Argument passed with ${arg.irType}, callable as ${argDecl.irType}")
+      }
+
+      (arg.toIrWithType :: argDecl.attributes.map(_.toIr).toList.sorted).mkString(" ")
+    } mkString(", ")
+
+    // Only noreturn, nounwind, readnone, and readonly are allowed here
+    val filteredFuncAttrs = callable.attributes.intersect(Set(IrFunction.NoReturn, IrFunction.NoUnwind, IrFunction.ReadNone, IrFunction.ReadOnly))
+    val funcAttrIrs = filteredFuncAttrs.map(_.toIr).toList.sorted
+
+    // Start string building
+    val callKernel = functionPtr.toIr + "(" + argIr + ")"
+
+    val callParts : List[String] =
+      assignmentIrOpt.toList ++ tailCallIrOpt.toList ++ List("call") ++ callingConvIrOpt.toList ++ retAttrIrs ++ List(resultTypeIr) ++ List(callKernel) ++ funcAttrIrs
+
+    instructions += callParts.mkString(" ")
+
+    resultVarOpt
+  }
 }
--- compiler/src/test/scala/codegen/llvmir/OtherInstrsSuite.scala
+++ compiler/src/test/scala/codegen/llvmir/OtherInstrsSuite.scala
@@ -47,5 +47,203 @@ class OtherInstrsSuite extends FunSuite {
 
     assert(block.toIr === "\t%1 = phi double [ 1.0, %plusone ], [ -2.0, %minustwo ]")
   }
+  
+  test("trivial call") {
+    val declResult = IrFunction.Result(VoidType, Set())
+    val declArgs = List()
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="doNothing",
+      arguments=declArgs)
+
+    val block = new IrBlock {
+      val resultVar = callDecl(
+        decl=decl,
+        arguments=List()
+      )
+
+      assert(resultVar === None)
+    }
+
+    assert(block.toIr === "\tcall void @doNothing()")
+  }
+  
+  test("call returning value") {
+    val declResult = IrFunction.Result(IntegerType(8), Set(IrFunction.ZeroExt))
+    val declArgs = List()
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="returnSomething",
+      arguments=declArgs)
+
+    val block = new IrBlock {
+      val resultVar = callDecl(
+        decl=decl,
+        arguments=List()
+      )
+
+      assert(resultVar.isDefined)
+    }
+
+    assert(block.toIr === "\t%1 = call zeroext i8 @returnSomething()")
+  }
+  
+  test("fastcc call") {
+    val declResult = IrFunction.Result(VoidType, Set())
+    val declArgs = List()
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="fastCc",
+      arguments=declArgs,
+      callingConv=CallingConv.FastCC)
+
+    val block = new IrBlock {
+      val resultVar = callDecl(
+        decl=decl,
+        arguments=List()
+      )
+
+      assert(resultVar === None)
+    }
+
+    assert(block.toIr === "\tcall fastcc void @fastCc()")
+  }
+  
+  test("tail call") {
+    val declResult = IrFunction.Result(VoidType, Set())
+    val declArgs = List()
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="doNothing",
+      arguments=declArgs)
+
+    val block = new IrBlock {
+      val resultVar = callDecl(
+        decl=decl,
+        arguments=List(),
+        tailCall=true
+      )
+
+      assert(resultVar === None)
+    }
+
+    assert(block.toIr === "\ttail call void @doNothing()")
+  }
+
+  test("call with insufficent args") {
+    val declResult = IrFunction.Result(VoidType, Set())
+    val declArgs = List(IrFunction.Argument(PointerType(IntegerType(8)), Set(IrFunction.NoCapture)))
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="notEnoughArgs",
+      arguments=declArgs)
+    
+    val block = new IrBlock {
+      intercept[InternalCompilerErrorException] {
+        callDecl(decl=decl, arguments=List())
+      }
+    }
+  }
+
+  test("call with unmatched args") {
+    val declResult = IrFunction.Result(VoidType, Set())
+    val declArgs = List(IrFunction.Argument(IntegerType(8), Set(IrFunction.NoCapture)))
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="mismatchedArgs",
+      arguments=declArgs)
+    
+    val block = new IrBlock {
+      val mismatchedValue = IntegerConstant(IntegerType(16), 5)
+
+      intercept[InternalCompilerErrorException] {
+        callDecl(
+          decl=decl,
+          arguments=List(mismatchedValue))
+      }
+    }
+  }
+
+  test("call with args") {
+    val declResult = IrFunction.Result(VoidType, Set())
+    val declArgs = List(
+      IrFunction.Argument(IntegerType(8), Set(IrFunction.NoCapture)),
+      IrFunction.Argument(PointerType(IntegerType(1)), Set())
+    )
+
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="withArgs",
+      arguments=declArgs)
+    
+    val block = new IrBlock {
+      val mismatchedValue = IntegerConstant(IntegerType(16), 5)
+
+      val resultVar = callDecl(
+        decl=decl,
+        arguments=List(
+          IntegerConstant(IntegerType(8), 1),
+          LocalVariable("local", PointerType(IntegerType(1)))
+        )
+      )
+    
+      assert(resultVar === None)
+    }
+    
+    assert(block.toIr === "\tcall void @withArgs(i8 1 nocapture, i1* %local)")
+  }
+  
+  test("call with attrs") {
+    val declResult = IrFunction.Result(VoidType, Set())
+    val declArgs = List()
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="withAttrs",
+      arguments=declArgs,
+      // IrFunction.Cold should be filtered out
+      attributes=Set(IrFunction.Cold, IrFunction.ReadOnly)
+    )
+
+    val block = new IrBlock {
+      val resultVar = callDecl(
+        decl=decl,
+        arguments=List()
+      )
+
+      assert(resultVar === None)
+    }
+
+    assert(block.toIr === "\tcall void @withAttrs() readonly")
+  }
+
+  test("christmas tree call") {
+    val declResult = IrFunction.Result(IntegerType(8), Set(IrFunction.ZeroExt))
+    val declArgs = List(
+      IrFunction.Argument(SingleType, Set(IrFunction.NoCapture, IrFunction.NoAlias)),
+      IrFunction.Argument(PointerType(DoubleType), Set())
+    )
+    val decl = IrFunctionDecl(
+      result=declResult,
+      name="uberCall",
+      arguments=declArgs,
+      attributes=Set(IrFunction.NoUnwind, IrFunction.NoReturn),
+      callingConv=CallingConv.ColdCC
+    )
+    
+    val block = new IrBlock {
+      val resultVar = callDecl(
+        decl=decl,
+        arguments=List(
+          SingleConstant(2.0f),
+          LocalVariable("local", PointerType(DoubleType))
+        ),
+        tailCall=true
+      )
+
+      assert(resultVar.isDefined)
+    }
+
+    assert(block.toIr === "\t%1 = tail call coldcc zeroext i8 @uberCall(float 2.0 noalias nocapture, double* %local) noreturn nounwind")
+  }
 }
 
