9aa291923a570731f1408e1c6fd5b3465c419fae
Ryan Cumming <etaoins@gmail.com>
2013-10-10 21:12:11 -0700

    Add support for setting and referencing vars
    This implicitly assumes all the variable are immutable. This is a simple
    implementation to get (define (native-function)) working in (scheme
    core) to build further unit tests.

--- compiler/src/main/scala/llambda/codegen/GenExpression.scala
+++ compiler/src/main/scala/llambda/codegen/GenExpression.scala
@@ -1,5 +1,6 @@
 package llambda.codegen
 
+import llambda.{StorageLocation, InternalCompilerErrorException}
 import llambda.et
 import llambda.codegen.llvmir._
 
@@ -10,6 +11,31 @@ case class ExpressionResult(
 object GenExpression {
   def apply(initialState : GenerationState)(expr : et.Expression) : ExpressionResult = {
     expr match {
+      case et.Bind(bindings) =>
+        val finalState = bindings.foldLeft(initialState) { case (state, (storageLoc, bindValue)) =>
+          val bindResult = GenExpression(state)(bindValue)
+
+          // Add the new variable to our liveVariables
+          val newLiveVars = bindResult.state.liveVariables + (storageLoc -> bindResult.value)
+
+          // Return the modified state
+          bindResult.state.copy(
+            liveVariables=newLiveVars
+          )
+        }
+
+        ExpressionResult(state=finalState, value=LiveUnspecific)
+        
+      case et.VarRef(storageLoc : StorageLocation) =>
+        // XXX: Can this fail?
+        ExpressionResult(
+          state=initialState,
+          value=initialState.liveVariables(storageLoc)
+        )
+
+      case et.VarRef(_) =>
+        throw new InternalCompilerErrorException("Non-StorageLocation VarRef leaked to codegen")
+
       case et.Literal(datum) =>
         ExpressionResult(state=initialState, value=GenLiteral(datum))
 
