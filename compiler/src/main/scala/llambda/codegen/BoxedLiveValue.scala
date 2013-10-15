fd00a5639f4477d2ab456412fe7414daeffe4f07
Ryan Cumming <etaoins@gmail.com>
2013-10-14 20:14:09 -0700

    Fix indentation in genCastToBoxed
    This method was indented one level too deep

--- compiler/src/main/scala/llambda/codegen/BoxedLiveValue.scala
+++ compiler/src/main/scala/llambda/codegen/BoxedLiveValue.scala
@@ -14,32 +14,32 @@ class BoxedLiveValue(boxedType : bt.BoxedType, boxedValue : IrValue) extends Liv
     None
 
   protected def genCastToBoxed(initialState : GenerationState)(targetType : bt.BoxedType) : Option[(GenerationState, IrValue)] = {
-      if (targetType.isTypeOrSupertypeOf(boxedType)) {
-        // This doesn't require any type assertions
-        val supertypeValue = targetType.genPointerBitcast(initialState.currentBlock)(boxedValue)
+    if (targetType.isTypeOrSupertypeOf(boxedType)) {
+      // This doesn't require any type assertions
+      val supertypeValue = targetType.genPointerBitcast(initialState.currentBlock)(boxedValue)
 
-        Some((initialState, supertypeValue))
-      }
-      else if (targetType.isTypeOrSubtypeOf(boxedType)) {
-        // We need to build a type assertion
-        val successBlock = initialState.currentBlock.startChildBlock(targetType.name + "SubcastSuccess") 
-        val failBlock = initialState.currentBlock.startChildBlock(targetType.name + "SubcastFail") 
+      Some((initialState, supertypeValue))
+    }
+    else if (targetType.isTypeOrSubtypeOf(boxedType)) {
+      // We need to build a type assertion
+      val successBlock = initialState.currentBlock.startChildBlock(targetType.name + "SubcastSuccess") 
+      val failBlock = initialState.currentBlock.startChildBlock(targetType.name + "SubcastFail") 
 
-        // Do the actual check
-        targetType.genTypeCheck(initialState.currentBlock)(boxedValue, successBlock, failBlock)
-      
-        // Generate the fail branch
-        val errorName = s"subcastFrom${boxedType.name.capitalize}To${targetType.name.capitalize}"
-        val errorMessage = s"Runtime cast from '${boxedType.name}' to subtype '${targetType.name}' failed" 
-        GenFatalError(initialState.module, failBlock)(errorName, errorMessage)
+      // Do the actual check
+      targetType.genTypeCheck(initialState.currentBlock)(boxedValue, successBlock, failBlock)
+    
+      // Generate the fail branch
+      val errorName = s"subcastFrom${boxedType.name.capitalize}To${targetType.name.capitalize}"
+      val errorMessage = s"Runtime cast from '${boxedType.name}' to subtype '${targetType.name}' failed" 
+      GenFatalError(initialState.module, failBlock)(errorName, errorMessage)
 
-        // Continue building on the success block
-        val subtypeValue = targetType.genPointerBitcast(successBlock)(boxedValue)
-        Some((initialState.copy(currentBlock=successBlock), subtypeValue))
-      }
-      else {
-        None
-      }
+      // Continue building on the success block
+      val subtypeValue = targetType.genPointerBitcast(successBlock)(boxedValue)
+      Some((initialState.copy(currentBlock=successBlock), subtypeValue))
+    }
+    else {
+      None
+    }
   }
 
   def toNativeType(state : GenerationState)(targetType : nfi.NativeType) : Option[(GenerationState, IrValue)] = {
