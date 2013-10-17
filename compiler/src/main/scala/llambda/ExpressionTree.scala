ef4242ccc81a4af1fa73443c202badce9ac07485
Ryan Cumming <etaoins@gmail.com>
2013-10-16 22:53:25 -0700

    Add et.Expression.map
    This applies a function to each of the subexpressions and then returns
    an expression containing those modified subexpressions. Along with
    et.Begin this should allow clean and monadic optimizer code.

--- compiler/src/main/scala/llambda/ExpressionTree.scala
+++ compiler/src/main/scala/llambda/ExpressionTree.scala
@@ -3,38 +3,64 @@ import llambda._
 
 sealed abstract trait Expression {
   val subexpressions : List[Expression]
+
+  def map(f : Expression => Expression) : Expression
 }
 
 case class Begin(expressions : List[Expression]) extends Expression {
   val subexpressions = expressions
+
+  def map(f : Expression => Expression) : et.Begin =
+    et.Begin(expressions.map(f))
 }
 
 case class Apply(procedure : Expression, operands : List[Expression]) extends Expression {
-  val subexpressions  = procedure :: operands
+  val subexpressions = procedure :: operands
+
+  def map(f : Expression => Expression) : et.Apply =
+    et.Apply(f(procedure), operands.map(f))
 }
 
 case class VarRef(variable : BoundValue) extends Expression {
   val subexpressions = Nil
+
+  def map(f : Expression => Expression) : et.VarRef = this
 }
 
 case class MutateVar(variable : StorageLocation, value : Expression) extends Expression {
   val subexpressions = value :: Nil
+
+  def map(f : Expression => Expression) : et.MutateVar =
+    et.MutateVar(variable, f(value))
 }
 
 case class Literal(value : ast.Datum) extends Expression {
   val subexpressions = Nil
+
+  def map(f : Expression => Expression) : et.Literal = this
 }
 
 case class Cond(test : Expression, trueExpr : Expression, falseExpr : Expression) extends Expression {
   val subexpressions = test :: trueExpr :: falseExpr :: Nil
+
+  def map(f : Expression => Expression) : et.Cond =
+    et.Cond(f(test), f(trueExpr), f(falseExpr))
 }
 
 case class Lambda(fixedArgs : List[StorageLocation], restArg : Option[StorageLocation], body : Expression) extends Expression {
   val subexpressions = body :: Nil
+
+  def map(f : Expression => Expression) : et.Lambda =
+    et.Lambda(fixedArgs, restArg, f(body))
 }
 
 case class Bind(bindings : List[(StorageLocation, Expression)]) extends Expression {
   val subexpressions = bindings.map(_._2)
+
+  def map(f : Expression => Expression) : et.Bind =
+    et.Bind(bindings.map { case (storageLoc, expr) =>
+      (storageLoc, f(expr))
+    })
 }
 
 case class NativeFunction(
@@ -46,4 +72,5 @@ case class NativeFunction(
   val hasSelfArg = false
 
   val subexpressions = Nil
+  def map(f : Expression => Expression) : et.NativeFunction = this
 }
