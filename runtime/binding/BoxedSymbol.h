c2dfabc5c1c8835f84960fb9ee59c040af428fb6
Ryan Cumming <etaoins@gmail.com>
2013-10-17 20:07:33 -0700

    Make BoxedSymbol include the right members
    It was previously including BoxedStringMembers probably due to a copy
    and paste error. Switch to BoxedSymbolMembers.

--- runtime/binding/BoxedSymbol.h
+++ runtime/binding/BoxedSymbol.h
@@ -8,7 +8,7 @@ namespace lliby
 
 class BoxedSymbol : public BoxedStringLike
 {
-#include "generated/BoxedStringMembers.h"
+#include "generated/BoxedSymbolMembers.h"
 public:
 	BoxedSymbol(std::uint8_t *utf8Data, std::uint32_t byteLength, std::uint32_t charLength) :
 		BoxedStringLike(BoxedTypeId::Symbol, utf8Data, byteLength, charLength)
