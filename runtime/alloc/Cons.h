a3d2cac7ccc52d94cb2c71fdef3c6468cfbaed73
Ryan Cumming <etaoins@gmail.com>
2013-12-03 17:59:27 -0800

    Add comment about sizecheck in Cons.h
    The existing comment was from the time when the size of the Cons cell
    was based on good luck and a positive attitude. Mention the fact we have
    compiler time checks now.

--- runtime/alloc/Cons.h
+++ runtime/alloc/Cons.h
@@ -10,6 +10,7 @@ namespace alloc
 
 // This is a placeholder for size purposes
 // We assume a PairCell is the largest allocation with two pointers
+// If this isn't true then sizecheck.h will assert at compile time
 class Cons : public PairCell
 {
 };
