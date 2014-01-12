67947497fc41ed67fb4d3e1b09fd479bd76c954b
Ryan Cumming <etaoins@gmail.com>
2014-01-12 15:13:32 -0800

    Add CondExpander + unit tests
    This is the backend engine for (cond-expand) support in
    ExtractModuleBody. It supports all of the syntax defined in R7RS

---
+++ compiler/src/main/scala/frontend/CondExpander.scala
@@ -0,0 +1,68 @@
+package io.llambda.compiler.frontend
+import io.llambda
+
+import scala.annotation.tailrec
+
+import llambda.compiler.sst
+import llambda.compiler.BadSpecialFormException
+
+private[frontend] object CondExpander {
+  private def requirementSatisfied(requirement : sst.ScopedDatum)(implicit libraryLoader : LibraryLoader, frontendConfig : FrontendConfig) : Boolean = requirement match {
+    case sst.ScopedSymbol(_, name) =>
+      frontendConfig.featureIdentifiers.contains(name)
+
+    case sst.ScopedProperList(List(sst.ScopedSymbol(_, "library"), libraryNameDatum)) =>
+      val parsedLibName = ParseLibraryName(libraryNameDatum.unscope)
+      libraryLoader.exists(parsedLibName)
+
+    case sst.ScopedProperList(List(sst.ScopedSymbol(_, "not"), innerRequirement)) =>
+      !requirementSatisfied(innerRequirement)
+    
+    case sst.ScopedProperList(sst.ScopedSymbol(_, "and") :: innerRequirements) =>
+      innerRequirements.foldLeft(true) { (accumulator, innerRequirement) =>
+        accumulator && requirementSatisfied(innerRequirement)
+      }
+    
+    case sst.ScopedProperList(sst.ScopedSymbol(_, "or") :: innerRequirements) =>
+      innerRequirements.foldLeft(false) { (accumulator, innerRequirement) =>
+        accumulator || requirementSatisfied(innerRequirement)
+      }
+
+    case other =>
+      throw new BadSpecialFormException(other, "Invalid requirement syntax")
+  }
+
+  @tailrec
+  def apply(clauseList : List[sst.ScopedDatum])(implicit libraryLoader : LibraryLoader, frontendConfig : FrontendConfig) : List[sst.ScopedDatum] = clauseList match {
+    case Nil => 
+      // No clauses left; expand to nothing. 
+      // The behaviour here is left open by R7RS but expanding to nothing will
+      // evalute to #!unit after being wrapped in an et.Begin which seems sane.
+      Nil
+
+    case sst.ScopedProperList((elseSymbol @ sst.ScopedSymbol(_, "else")) :: expandData) :: tailClauses =>
+      if (tailClauses.isEmpty) {
+        // We hit an else clause as our last clause
+        // Expand the else
+        expandData
+      }
+      else {
+        // Else was *not* the last clause
+        // Don't treat it like a feature identifier
+        throw new BadSpecialFormException(elseSymbol, "The else clause must be the last (cond-expand) clause")
+      }
+
+    case sst.ScopedProperList(requirement :: expandData) :: tailClauses =>
+      if (requirementSatisfied(requirement)) {
+        // Requirement satisfied!
+        expandData
+      }
+      else {
+        // Keep scanning the clauses
+        apply(tailClauses)
+      }
+
+    case other :: _ =>
+      throw new BadSpecialFormException(other, "(cond-expand) clauses must be proper lists consisting of a requirement followed by zero or more expressions to expand")
+  }
+}
---
+++ compiler/src/test/scala/frontend/CondExpanderSuite.scala
@@ -0,0 +1,291 @@
+package io.llambda.compiler.frontend
+import io.llambda
+
+import org.scalatest.FunSuite
+
+import llambda.compiler._
+
+class CondExpanderSuite extends FunSuite {
+  private val primitiveScope = new Scope(collection.mutable.Map(PrimitiveExpressions.bindings.toSeq : _*))
+  private val libraryLoader = new LibraryLoader(platform.Posix64LE)
+
+  private def expansionFor(clauseList : List[String], featureIdentifiers : Set[String]) : List[ast.Datum] = {
+    val parsedClauses = clauseList.map(SchemeParser.parseStringAsData(_, None).head)
+    val scopedClauses = parsedClauses.map(sst.ScopedDatum(primitiveScope, _))
+
+    val frontendConfig = FrontendConfig(
+      includePath=IncludePath(),
+      featureIdentifiers=featureIdentifiers
+    )
+
+    CondExpander(scopedClauses)(libraryLoader, frontendConfig).map(_.unscope)
+  }
+  
+  test("non-proper list clause fails") {
+    intercept[BadSpecialFormException] {
+      expansionFor(
+        List(
+          """my-feature"""
+        ),
+        Set(
+          """my-feature"""
+        )
+      )
+    }
+  }
+  
+  test("empty proper list clause fails") {
+    intercept[BadSpecialFormException] {
+      expansionFor(
+        List(
+          """()"""
+        ),
+        Set(
+          """my-feature"""
+        )
+      )
+    }
+  }
+  
+  test("empty expansion when no conditions match and no else") {
+    val data = expansionFor(
+      List(
+        """(this-is-not-a-feature 1 2 3)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === Nil)
+  }
+  
+  test("else clause fallaback no conditions match") {
+    val data = expansionFor(
+      List(
+        """(this-is-not-a-feature 1 2 3)""",
+        """(else 4 5 6)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === List(4, 5, 6).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("non-terminal else clause fails") {
+    intercept[BadSpecialFormException] {
+      expansionFor(
+        List(
+          """(else 4 5 6)""",
+          """(this-is-not-a-feature 1 2 3)"""
+        ),
+        Set(
+          """my-feature"""
+        )
+      )
+    }
+  }
+
+  test("trivial feature identifier expansion") {
+    val data = expansionFor(
+      List(
+        """(my-feature 1 2 3)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("first matching clause is expanded") {
+    val data = expansionFor(
+      List(
+        """(feature-1 1 2 3)""",
+        """(feature-2 4 3 6)"""
+      ),
+      Set(
+        """feature-1""",
+        """feature-2"""
+      )
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("trivial fallback feature identifier expansion") {
+    val data = expansionFor(
+      List(
+        """(not-a-feature 1 2 3)""",
+        """(my-feature 4 5 6)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === List(4, 5, 6).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("trivial existing library expansion") {
+    val data = expansionFor(
+      List(
+        """((library (scheme base)) 1 2 3)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("non-existing library expansion") {
+    val data = expansionFor(
+      List(
+        """((library (not a library)) 1 2 3)""",
+        """((library (scheme base)) 4 5 6)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === List(4, 5, 6).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("empty (and) condition is true") {
+    val data = expansionFor(
+      List(
+        """((and) 1 2 3)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("(and) with two true requirements") {
+    val data = expansionFor(
+      List(
+        """((and feature-1 feature-2) 1 2 3)"""
+      ),
+      Set(
+        """feature-1""",
+        """feature-2"""
+      )
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("(and) with one true and one false requirement") {
+    val data = expansionFor(
+      List(
+        """((and feature-1 not-a-feature) 1 2 3)"""
+      ),
+      Set(
+        """feature-1"""
+      )
+    )
+
+    assert(data === Nil)
+  }
+  
+  test("empty (or) condition is false") {
+    val data = expansionFor(
+      List(
+        """((or) 1 2 3)"""
+      ),
+      Set(
+        """my-feature"""
+      )
+    )
+
+    assert(data === Nil)
+  }
+  
+  test("(or) with two true requirements") {
+    val data = expansionFor(
+      List(
+        """((or feature-1 feature-2) 1 2 3)"""
+      ),
+      Set(
+        """feature-1""",
+        """feature-2"""
+      )
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("(or) with one true and one false requirement") {
+    val data = expansionFor(
+      List(
+        """((or feature-1 not-a-feature) 1 2 3)"""
+      ),
+      Set(
+        """feature-1"""
+      )
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+  
+  test("empty (not) fails") {
+    intercept[BadSpecialFormException] {
+      expansionFor(
+        List(
+          """((not) 1 2 3)"""
+        ),
+        Set(
+          """my-feature"""
+        )
+      )
+    }
+  }
+  
+  test("(not) with true value") {
+    val data = expansionFor(
+      List(
+        """((not feature-1) 1 2 3)"""
+      ),
+      Set(
+        """feature-1"""
+      )
+    )
+
+    assert(data === Nil)
+  }
+  
+  test("(not) with false value") {
+    val data = expansionFor(
+      List(
+        """((not not-a-feature) 1 2 3)"""
+      ),
+      Set()
+    )
+
+    assert(data === List(1, 2, 3).map(ast.IntegerLiteral(_)))
+  }
+
+  test("nested conditionals") {
+    val data = expansionFor(
+      List(
+        """((and feature-1 (not (library (scheme base)))) 1 2 3)""",
+        """((or (not feature-1) (and feature-2)) 4 5 6)"""
+      ),
+      Set(
+        """feature-1""",
+        """feature-2"""
+      )
+    )
+
+    assert(data === List(4, 5, 6).map(ast.IntegerLiteral(_)))
+  }
+}
