43bae2deea2487fa9d58cb6064cfe7cb481cabb3
Ryan Cumming <rcumming@ea.com>
2014-01-13 19:12:14 -0800

    Add functional test for (featurs)
    This simply checks for the presence of "r7rs" and "llambda"
    
    Closes #6

---
+++ compiler/src/test/scheme/functional/SystemInterfaceSuite.scm
@@ -0,0 +1,5 @@
+(define-test "(features) returns r7rs" (expect #t
+	(if (memv "r7rs" (features)) #t #f))) 
+
+(define-test "(features) returns llambda" (expect #t
+	(if (memv "llambda" (features)) #t #f))) 
