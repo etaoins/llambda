cdf5cd871c0615850573ae4476e9b1666809c5c8
Ryan Cumming <rcumming@ea.com>
2014-01-20 18:41:13 -0800

    Release resources in lliby_apply before calling
    Do everything inside a block so the ProcedureCell/StrongRefRange are
    destructed before calling the procedure. This reduces the resources we
    use during recursive calls to (apply) and might allow tail call
    optimization.

--- runtime/stdlib/control.cpp
+++ runtime/stdlib/control.cpp
@@ -14,48 +14,56 @@ extern "C"
 
 DatumCell *lliby_apply(ProcedureCell *procedure, ListElementCell *argHead)
 {
-	// Find our arguments
-	ProperList<DatumCell> applyArgList(argHead);
+	ListElementCell *procArgHead;
 
-	if (!applyArgList.isValid())
+	// Do everything inside the block so the ProperList/StrongRefRange is destructed before calling the procedure
+	// This reduces the resources we use during recursive calls to (apply) and might allow the compiler to perform
+	// tail call optimization
 	{
-		_lliby_fatal("Non-list passed to (apply)", argHead);
+		// Find our arguments
+		ProperList<DatumCell> applyArgList(argHead);
+
+		if (!applyArgList.isValid())
+		{
+			_lliby_fatal("Non-list passed to (apply)", argHead);
+		}
+		else if (applyArgList.length() == 0)
+		{
+			// This is easy - call with no args
+			procArgHead = argHead;
+		}
+		else
+		{
+			// Build our procedure args
+			auto applyArgIt = applyArgList.begin();
+			
+			// Standalone args are zero or more args that appear before the final final proper list
+			auto standaloneArgCount = applyArgList.length() - 1;
+			std::vector<DatumCell*> standaloneArgs(standaloneArgCount);
+
+			for(auto i = 0; i < standaloneArgCount; i++)
+			{
+				standaloneArgs[i] = *(applyArgIt++);
+			}
+
+			// Ensure the final argument is a proper list
+			// This would violate our calling convention otherwise
+			auto finalListHead = datum_cast<ListElementCell>(*applyArgIt);
+
+			if (!(finalListHead && ProperList<DatumCell>(finalListHead).isValid()))
+			{
+				_lliby_fatal("Final argument to (apply) must be a proper list", finalListHead);
+			}
+
+			// Reference the procedure cell before allocating the argument list
+			alloc::StrongRefRange<ProcedureCell> procedureRef(&procedure, 1);	
+
+			// We verified the final arg is a proper list so this must also be a proper list
+			procArgHead = static_cast<ListElementCell*>(ListElementCell::createList(standaloneArgs, finalListHead));
+		}
 	}
 
-	if (applyArgList.length() == 0)
-	{
-		// This is easy - call with no args
-		return procedure->apply(argHead);
-	}
-	
-	// Build our procedure args
-	auto applyArgIt = applyArgList.begin();
-	
-	// Standalone args are zero or more args that appear before the final final proper list
-	auto standaloneArgCount = applyArgList.length() - 1;
-	std::vector<DatumCell*> standaloneArgs(standaloneArgCount);
-
-	for(auto i = 0; i < standaloneArgCount; i++)
-	{
-		standaloneArgs[i] = *(applyArgIt++);
-	}
-
-	// Ensure the final argument is a proper list
-	// This would violate our calling convention otherwise
-	auto finalListHead = datum_cast<ListElementCell>(*applyArgIt);
-
-	if (!(finalListHead && ProperList<DatumCell>(finalListHead).isValid()))
-	{
-		_lliby_fatal("Final argument to (apply) must be a proper list", finalListHead);
-	}
-
-	// Reference the procedure before allocating the argument list
-	alloc::StrongRef<ProcedureCell> procedureRef(procedure);	
-
-	// We verified the final arg is a proper list so this must also be a proper list
-	auto procArgHead = static_cast<ListElementCell*>(ListElementCell::createList(standaloneArgs, finalListHead));
-
-	return procedureRef->apply(procArgHead);
+	return procedure->apply(procArgHead);
 }
 
 }
