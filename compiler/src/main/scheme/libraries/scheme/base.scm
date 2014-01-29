f2acb546cf0891b9d67e72e84870888ace6ca786
Ryan Cumming <etaoins@gmail.com>
2014-01-29 14:13:53 -0800

    Add (cond) and (case) support
    These macros are identical to the example definitions in R7RS. These are
    useful because they appear frequently in R7RS examples and are a good
    litmus test for macro parsing and other compiler features.

--- compiler/src/main/scheme/libraries/scheme/base.scm
+++ compiler/src/main/scheme/libraries/scheme/base.scm
@@ -24,8 +24,65 @@
 							 ((lambda (name ...) body1 body2 ...)
 							  val ...)))))
 
-	(export and or when)
+	(export cond case and or when)
 	(begin
+	  (define-syntax cond
+		 (syntax-rules (else =>)
+							((cond (else result1 result2 ...))
+							 (begin result1 result2 ...))
+							((cond (test => result))
+							 (let ((temp test))
+								(if temp (result temp))))
+							((cond (test => result) clause1 clause2 ...)
+							 (let ((temp test))
+								(if temp
+								  (result temp)
+								  (cond clause1 clause2 ...))))
+							((cond (test)) test)
+							((cond (test) clause1 clause2 ...)
+							 (let ((temp test))
+								(if temp temp
+								  (cond clause1 clause2 ...))))
+							((cond (test result1 result2 ...))
+							 (if test (begin result1 result2 ...)))
+							((cond (test result1 result2 ...)
+									 clause1 clause2 ...)
+							 (if test
+								(begin result1 result2 ...)
+								(cond clause1 clause2 ...)))))
+	  (define-syntax case
+		 (syntax-rules (else =>)
+							((case (key ...)
+								clauses ...)
+							 (let ((atom-key (key ...)))
+								(case atom-key clauses ...)))
+							((case key
+								(else => result))
+							 (result key))
+							((case key
+								(else result1 result2 ...))
+							 (begin result1 result2 ...))
+							((case key
+								((atoms ...) result1 result2 ...))
+							 (if (memv key '(atoms ...))
+								(begin result1 result2 ...)))
+							((case key
+								((atoms ...) => result))
+							 (if (memv key '(atoms ...))
+								(result key)))
+							((case key
+								((atoms ...) => result)
+								clause clauses ...)
+							 (if (memv key '(atoms ...))
+								(result key)
+								(case key clause clauses ...)))
+							((case key
+								((atoms ...) result1 result2 ...)
+								clause clauses ...)
+							 (if (memv key '(atoms ...))
+								(begin result1 result2 ...)
+								(case key clause clauses ...)))))
+
 	  (define-syntax and
 		 (syntax-rules ()
 							((and) #t)
--- compiler/src/test/scheme/functional/ConditionalSuite.scm
+++ compiler/src/test/scheme/functional/ConditionalSuite.scm
@@ -1,3 +1,38 @@
+(define-test "(cond) without arrows or else" (expect true 
+	(cond (#f 'false)
+			(#t 'true))))
+
+(define-test "(cond) with arrows, without else" (expect #f
+	(cond (#f => not)
+			; This becomes (not #t)
+			(#t => not))))
+
+(define-test "(cond) without arrows, with else" (expect else
+	(cond (#f 'false1)
+			(#f 'false2)
+			(else 'else))))
+
+(define-test "(cond) with arrows and else" (expect else
+	(cond (#f => not)
+			(#f => not)
+			(else 'else))))
+
+(define-test "(case) matching clause" (expect composite
+	(case (* 2 3)
+	  ((2 3 5 7) 'prime)
+	  ((1 4 6 8 9) 'composite))))
+
+(define-test "(case) without matching clause" (expect #!unit
+	(case (car '(c d)) 
+	  ((a) 'a)
+	  ((b) 'b))))
+
+(define-test "(case) with arrow" (expect c
+	(case (car '(c d))
+	  ((a e i o u) 'vowel)
+	  ((w y) 'semivowel)
+	  (else => (lambda (x) x)))))
+
 (define-test "empty (and) evaluates to true" (expect #t
 	(and)))
 
