78d7335ed370dc9bd58af1512a51f31ccf26a817
Ryan Cumming <etaoins@gmail.com>
2014-07-05 12:26:48 +1000

    Add full-test.sh
    This runs the runtime and Scala tests with 3 different runtime
    configurations. This will make it easier to verify Llambda on various
    platforms.

---
+++ full-test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# If anything fails we should fail
+set -e
+
+BUILD_DIR=build/
+
+rm -Rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+# List of configurations of the runtime to test
+RUNTIME_CONFIGURATIONS=("-DCMAKE_BUILD_TYPE=Debug -DENABLE_GC_DEBUGGING=yes"
+                        "-DCMAKE_BUILD_TYPE=Debug -DENABLE_GC_DEBUGGING=no"
+                        "-DCMAKE_BUILD_TYPE=Release")
+
+for config in "${RUNTIME_CONFIGURATIONS[@]}"
+do
+	cmake -GNinja $config ../runtime
+	ninja
+
+	# Run time runtime tests
+	ninja test
+
+	cd ..
+
+	# Run the Scala tests
+	sbt test
+
+	cd -
+done
