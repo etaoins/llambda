5e6d49cd63dcd53ef4c51e5c23391cccd010b8f0
Ryan Cumming <etaoins@gmail.com>
2014-07-17 20:50:14 +1000

    compiler: Remove LocatedSemanticException
    All SemanticExceptions are now located. Collapse SemanticException and
    LocatedSemanticException in to one sealed class to simplify the code.

--- compiler/src/main/scala/SemanticException.scala
+++ compiler/src/main/scala/SemanticException.scala
@@ -1,19 +1,16 @@
 package io.llambda.compiler
 import io.llambda
 
-abstract class SemanticException(message : String) extends Exception(message) {
+sealed abstract class SemanticException(val located : SourceLocated, message : String) extends 
+    Exception(message + "\n" + located.locationString) {
   val semanticErrorType : String
 }
 
-sealed abstract class LocatedSemanticException(val located : SourceLocated, message : String) extends 
-  SemanticException(message + "\n" + located.locationString) {
-}
-
-class DubiousLibraryNameComponentException(located : SourceLocated, val libraryName : String) extends LocatedSemanticException(located, libraryName) {
+class DubiousLibraryNameComponentException(located : SourceLocated, val libraryName : String) extends SemanticException(located, libraryName) {
   val semanticErrorType = "dubious library name"
 }
 
-abstract class ReferencedFileNotFoundException(located : SourceLocated, val filename : String) extends LocatedSemanticException(located, filename)
+abstract class ReferencedFileNotFoundException(located : SourceLocated, val filename : String) extends SemanticException(located, filename)
 
 class LibraryNotFoundException(located : SourceLocated, filename : String) extends ReferencedFileNotFoundException(located, filename) {
   val semanticErrorType = "library not found"
@@ -24,49 +21,49 @@ class IncludeNotFoundException(located : SourceLocated, filename : String) exten
 }
 
 class LibraryNameMismatchException(located : SourceLocated, val loadedName : Seq[LibraryNameComponent], val definedName : List[LibraryNameComponent]) extends
-  LocatedSemanticException(located, "(" + loadedName.mkString(" ") + ") doesn't match (" + definedName.mkString(" ") + ")") {
+  SemanticException(located, "(" + loadedName.mkString(" ") + ") doesn't match (" + definedName.mkString(" ") + ")") {
   val semanticErrorType = "library name mismatch"
 }
 
-case class InvalidLibraryNameException(val datum : ast.Datum) extends LocatedSemanticException(datum, datum.toString) {
+case class InvalidLibraryNameException(val datum : ast.Datum) extends SemanticException(datum, datum.toString) {
   val semanticErrorType = "invalid library name"
 }
 
-class NoSyntaxRuleException(located : SourceLocated, message : String) extends LocatedSemanticException(located, message) {
+class NoSyntaxRuleException(located : SourceLocated, message : String) extends SemanticException(located, message) {
   val semanticErrorType = "no syntax rule"
 }
 
-class MalformedExprException(located : SourceLocated, message : String) extends LocatedSemanticException(located, message) {
+class MalformedExprException(located : SourceLocated, message : String) extends SemanticException(located, message) {
   val semanticErrorType = "malformed expression"
 }
 
-class BadSpecialFormException(located : SourceLocated, message : String) extends LocatedSemanticException(located, message) {
+class BadSpecialFormException(located : SourceLocated, message : String) extends SemanticException(located, message) {
   val semanticErrorType = "bad special form"
 }
 
-class UnboundVariableException(located : SourceLocated, val variableName : String) extends LocatedSemanticException(located, variableName) {
+class UnboundVariableException(located : SourceLocated, val variableName : String) extends SemanticException(located, variableName) {
   val semanticErrorType = "unbound variable"
 }
 
 class UserDefinedSyntaxError(located : SourceLocated, val errorString  : String, val data : List[ast.Datum])
-  extends LocatedSemanticException(located, errorString + " " + data.map(_.toString).mkString(" ")) {
+  extends SemanticException(located, errorString + " " + data.map(_.toString).mkString(" ")) {
 
   val semanticErrorType = "user defined syntax error"
 }
 
-class ImportedIdentifierNotFoundException(located : SourceLocated, val identifier : String) extends LocatedSemanticException(located, identifier) {
+class ImportedIdentifierNotFoundException(located : SourceLocated, val identifier : String) extends SemanticException(located, identifier) {
   val semanticErrorType = "imported identifier not found"
 }
 
-class ValueNotApplicableException(located : SourceLocated, typeDescription : String) extends LocatedSemanticException(located, s"${typeDescription.capitalize} not applicable") {
+class ValueNotApplicableException(located : SourceLocated, typeDescription : String) extends SemanticException(located, s"${typeDescription.capitalize} not applicable") {
   val semanticErrorType = "not applicable"
 }
 
-class ImpossibleTypeConversionException(located : SourceLocated, message : String) extends LocatedSemanticException(located, message) {
+class ImpossibleTypeConversionException(located : SourceLocated, message : String) extends SemanticException(located, message) {
   val semanticErrorType = "impossible type conversion"
 }
 
-class IncompatibleArityException(located : SourceLocated, message : String) extends LocatedSemanticException(located, message) { 
+class IncompatibleArityException(located : SourceLocated, message : String) extends SemanticException(located, message) { 
   val semanticErrorType = "incompatible arity"
 }
 
