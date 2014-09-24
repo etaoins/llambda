dcfd5e89c2142f0c7c58243a856ac9953e5e047d
Ryan Cumming <etaoins@gmail.com>
2014-09-24 18:38:04 +1000

    stdlib: Add functional tests from SRFI-45
    SRFI-45 is the SRFI describing what eventually became the (scheme lazy)
    library in R7RS. It contains a lot of useful tests, examples and
    explanation that doesn't appear in R7RS
    
    - Add the memoization and reentrancy tests from SRFI-45. These were
      modified to place top level definitions before their first use and use
      (delay-force) instead of (lazy).
    
      "Reentrancy test 1" in included almost verbatim in our functional
      tests via R7RS. It's omitted to prevent duplication.
    
    - Add a test using (from), (stream-filter) and (stream-ref) from SRFI-45

--- compiler/src/test/scheme/functional/LazySuite.scm
+++ compiler/src/test/scheme/functional/LazySuite.scm
@@ -73,3 +73,108 @@
 
   (define p (delay (+ 1 2)))
   (assert-true ((make-predicate <promise>) p))))
+
+(define-test "SRFI-45 memoization test 1" (expect-output (hello)
+  (import (scheme lazy))
+  (import (scheme write))
+
+  (define s (delay (begin (write 'hello) 1)))
+
+  (force s)
+  (force s)))
+
+(define-test "SRFI-45 memoization test 2" (expect-output (bonjour)
+  (import (scheme lazy))
+  (import (scheme write))
+
+  (let ((s (delay (begin (display 'bonjour) 2))))
+    (+ (force s) (force s)))))
+
+(define-test "SRFI-45 memoization test 3" (expect-output (hi)
+  (import (scheme lazy))
+  (import (scheme write))
+
+  (define r (delay (begin (display 'hi) 1)))
+  (define s (delay-force r))
+  (define t (delay-force s))
+
+  (force t)
+  (force r)))
+
+; SRFI-45 reentry test 1 is essentially the same as "(force) only computes a value once"
+
+(define-test "SRFI-45 reentrancy test 2" (expect-success
+  (import (scheme lazy))
+  (import (scheme write))
+
+  (define f
+    (let ((first? #t))
+      (delay
+        (if first?
+          (begin
+            (set! first? #f)
+            (force f))
+          'second))))
+
+  (assert-equal 'second (force f))))
+
+(define-test "SRFI-45 reentrancy test 3" (expect-success
+  (import (scheme lazy))
+  (import (scheme write))
+
+  (define q
+    (let ((count 5))
+      (define (get-count) count)
+      (define p (delay (if (<= count 0)
+                         count
+                         (begin (set! count (- count 1))
+                                (force p)
+                                (set! count (+ count 2))
+                                count))))
+      (list get-count p)))
+  (define get-count (car q))
+  (define p (cadr q))
+
+  (assert-equal 5 (get-count))
+  (assert-equal 0 (force p))
+  (assert-equal 10 (get-count))))
+
+(define-test "SRFI-45 example procedures" (expect-success
+  (import (scheme lazy))
+
+  (define-syntax match
+    (syntax-rules ()
+                  ((match exp 
+                          (()      exp1)
+                          ((h . t) exp2))
+                   (let ((lst exp))
+                     (cond ((null? lst) exp1)
+                           ((pair? lst) (let ((h (car lst))
+                                              (t (cdr lst)))
+                                          exp2))
+                           (else 'match-error))))))
+
+  (define (from n)
+      (delay (cons n (from (+ n 1)))))
+
+  (define (stream-filter p? s)
+    (delay-force (match (force s)
+                 (()      (delay '())) 
+                 ((h . t) (if (p? h)
+                            (delay (cons h (stream-filter p? t)))
+                            (stream-filter p? t))))))
+
+  (define (stream-ref s index)
+    (delay-force
+      (match (force s)
+             (()      'error)
+             ((h . t) (if (zero? index)
+                        (delay h)
+                        (stream-ref t (- index 1)))))))
+
+  ; Check that evenness is correctly implemented - should terminate:
+  (assert-equal 0 (force (stream-ref (stream-filter zero? (from 0)) 0)))
+
+  (define s (stream-ref (from 0) 6))
+  (assert-true (promise? s))
+  (assert-equal 6 (force s))))
