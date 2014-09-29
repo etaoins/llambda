1e552ae1653e8c954ad3b890bfdb0dcd694546f9
Ryan Cumming <etaoins@gmail.com>
2014-09-29 20:13:34 +1000

    compiler: Fix typo

--- compiler/src/main/scala/planner/AdapterProcType.scala
+++ compiler/src/main/scala/planner/AdapterProcType.scala
@@ -7,5 +7,5 @@ import llambda.compiler.{celltype => ct}
 /** Value field of procedure adapters */
 case object AdapterProcField extends vt.RecordField("targetProc", vt.SchemeTypeAtom(ct.ProcedureCell))
 
-/** Adapter procedures are implemented as single field clsosures */
+/** Adapter procedures are implemented as single field closures */
 case object AdapterProcType extends vt.ClosureType("procAdapter", List(AdapterProcField))
