9baabf423d8f96518ddff764e7da9b6d676df17b
Ryan Cumming <etaoins@gmail.com>
2014-10-27 20:18:02 +1100

    stdlib: Reorganise Portsuite tests
    Use assertions instead of multiple standalone tests. This should make it
    more manageable to expand the tests in the future.

--- compiler/src/test/scheme/functional/PortSuite.scm
+++ compiler/src/test/scheme/functional/PortSuite.scm
@@ -1,29 +1,17 @@
-(define-test "current input port is a port" (expect #t
-  (port? (current-input-port))))
+(define-test "(current-input-port)" (expect-success
+  (assert-true  (port? (current-input-port)))
+  (assert-true  (input-port? (current-input-port)))
+  (assert-false (output-port? (current-input-port)))))
 
-(define-test "current output port is a port" (expect #t
-  (port? (current-output-port))))
+(define-test "(current-output-port)" (expect-success
+  (assert-true  (port? (current-output-port)))
+  (assert-false (input-port? (current-output-port)))
+  (assert-true  (output-port? (current-output-port)))))
 
-(define-test "current error port is a port" (expect #t
-  (port? (current-error-port))))
-
-(define-test "current input port is an input port" (expect #t
-  (input-port? (current-input-port))))
-
-(define-test "current output port is not an input port" (expect #f
-  (input-port? (current-output-port))))
-
-(define-test "current error port is not an input port" (expect #f
-  (input-port? (current-error-port))))
-
-(define-test "current input port is not a ouput port" (expect #f
-  (output-port? (current-input-port))))
-
-(define-test "current output port is a output port" (expect #t
-  (output-port? (current-output-port))))
-
-(define-test "current error port is a output port" (expect #t
-  (output-port? (current-error-port))))
+(define-test "(current-error-port)" (expect-success
+  (assert-true  (port? (current-error-port)))
+  (assert-false (input-port? (current-error-port)))
+  (assert-true  (output-port? (current-error-port)))))
 
 (define-test "writing to an input port fails" (expect-failure
   (import (scheme write))
