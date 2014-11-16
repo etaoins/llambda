88ea47346cfc1bc16f4aa5e203198a3093942f05
Ryan Cumming <etaoins@gmail.com>
2014-11-16 22:07:16 +1100

    stdlib: Add (llambda complex-stub)
    This has the same interface as (scheme complex). However, as numbers
    with imaginary parts are not supported it's of little practical use.
    It's not named (scheme complex) to prevent programs doing feature
    detection with (cond-expand) from determining the complex numbers are
    supported.
    
    This is mainly intended to be re-exported as part of the r5rs library

---
+++ compiler/src/main/scheme/libraries/llambda/complex-stub.scm
@@ -0,0 +1,35 @@
+(define-library (llambda complex-stub)
+  (import (rename (llambda internal primitives) (define-report-procedure define-r7rs)))
+  (import (scheme base))
+  (import (only (scheme inexact) acos))
+  (import (llambda typed))
+
+  (export make-rectangular make-polar real-part imag-part magnitude angle)
+
+  (begin
+    (define-r7rs make-rectangular (lambda: ([real : <number>] [imag : <number>])
+      (unless (zero? imag)
+        (error "Complex numbers are not supported"))
+      real))
+
+    (define-r7rs make-polar (lambda: ([mag : <number>] [ang : <number>])
+      (unless (zero? ang)
+        (error "Complex numbers are not supported"))
+      mag))
+
+    (define-r7rs real-part (lambda: ([num : <number>])
+      num))
+
+    (define-r7rs imag-part (lambda: ([num : <number>])
+      (if (equal? num +nan.0) +nan.0 0)))
+
+    (define-r7rs magnitude (lambda: ([num : <number>])
+      (abs num)))
+
+    (define-r7rs angle (lambda: ([num : <number>])
+      (cond
+        ((equal? num +nan.0) +nan.0)
+        ((zero? num) (error "(angle) is undefined for 0"))
+        ((positive? num) 0)
+        ; pi
+        (else (* 2 (acos 0))))))))
---
+++ compiler/src/test/scheme/functional/ComplexSuite.scm
@@ -0,0 +1,98 @@
+(define-test "(make-rectangular)" (expect-success
+  (import (llambda complex-stub))
+
+  (assert-equal 5 (make-rectangular 5 0))
+  (assert-equal -5 (make-rectangular -5 0))
+  (assert-equal 5.0 (make-rectangular 5.0 0))
+  (assert-equal -5.0 (make-rectangular -5.0 0))
+
+  (assert-equal 0.0 (make-rectangular 0.0 0))
+  (assert-equal -0.0 (make-rectangular -0.0 0))
+
+  (assert-equal +inf.0 (make-rectangular +inf.0 0))
+  (assert-equal -inf.0 (make-rectangular -inf.0 0))
+
+  (assert-equal +nan.0 (make-rectangular +nan.0 0))))
+
+(define-test "(make-polar)" (expect-success
+  (import (llambda complex-stub))
+
+  (assert-equal 5 (make-polar 5 0))
+  (assert-equal -5 (make-polar -5 0))
+  (assert-equal 5.0 (make-polar 5.0 0))
+  (assert-equal -5.0 (make-polar -5.0 0))
+
+  (assert-equal 0.0 (make-polar 0.0 0))
+  (assert-equal -0.0 (make-polar -0.0 0))
+
+  (assert-equal +inf.0 (make-polar +inf.0 0))
+  (assert-equal -inf.0 (make-polar -inf.0 0))
+
+  (assert-equal +nan.0 (make-polar +nan.0 0))))
+
+(define-test "(real-part)" (expect-success
+  (import (llambda complex-stub))
+
+  (assert-equal 5 (real-part 5))
+  (assert-equal -5 (real-part -5))
+  (assert-equal 5.0 (real-part 5.0))
+  (assert-equal -5.0 (real-part -5.0))
+
+  (assert-equal 0.0 (real-part 0.0))
+  (assert-equal -0.0 (real-part -0.0))
+
+  (assert-equal +inf.0 (real-part +inf.0))
+  (assert-equal -inf.0 (real-part -inf.0))
+
+  (assert-equal +nan.0 (real-part +nan.0))))
+
+(define-test "(imag-part)" (expect-success
+  (import (llambda complex-stub))
+
+  (assert-equal 0 (imag-part 5))
+  (assert-equal 0 (imag-part -5))
+  (assert-equal 0 (imag-part 5.0))
+  (assert-equal 0 (imag-part -5.0))
+
+  (assert-equal 0 (imag-part 0.0))
+  (assert-equal 0 (imag-part -0.0))
+
+  (assert-equal 0 (imag-part +inf.0))
+  (assert-equal 0 (imag-part -inf.0))
+
+  (assert-equal +nan.0 (imag-part +nan.0))))
+
+(define-test "(magnitude)" (expect-success
+  (import (llambda complex-stub))
+
+  (assert-equal 5 (magnitude 5))
+  (assert-equal 5 (magnitude -5))
+  (assert-equal 5.0 (magnitude 5.0))
+  (assert-equal 5.0 (magnitude -5.0))
+
+  (assert-equal 0.0 (magnitude 0.0))
+  (assert-equal 0.0 (magnitude -0.0))
+
+  (assert-equal +inf.0 (magnitude +inf.0))
+  (assert-equal +inf.0 (magnitude -inf.0))
+
+  (assert-equal +nan.0 (magnitude +nan.0))))
+
+(define-test "(angle)" (expect-success
+  (import (llambda complex-stub))
+
+  (define pi 3.14159)
+
+  (assert-equal 0 (angle 5))
+  (assert-within pi 0.001 (angle -5))
+  (assert-equal 0 (angle 5.0))
+  (assert-within pi 0.001 (angle -5.0))
+
+  ; (angle) is undefined for zero
+  (assert-raises error-object? (angle 0.0))
+  (assert-raises error-object? (angle -0.0))
+
+  (assert-equal 0 (angle +inf.0))
+  (assert-within pi 0.001 (angle -inf.0))
+
+  (assert-equal +nan.0 (angle +nan.0))))
