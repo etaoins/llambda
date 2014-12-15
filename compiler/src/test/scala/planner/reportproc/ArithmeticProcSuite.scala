51d67089cade18688ab32889bd7db0793ce0c02f
Ryan Cumming <etaoins@gmail.com>
2014-12-15 19:28:09 +1100

    compiler: Split {Arithmetic, Number}ProcSuite
    This matches the split that was done in the stdlib and reportproc a
    while ago

---
+++ compiler/src/test/scala/planner/reportproc/ArithmeticProcSuite.scala
@@ -0,0 +1,137 @@
+package io.llambda.compiler.planner.reportproc
+import io.llambda
+
+import llambda.compiler.ast
+import llambda.compiler.planner.PlanHelpers
+import org.scalatest.FunSuite
+import llambda.compiler.DivideByZeroException
+
+class ArithmeticProcSuite extends FunSuite with PlanHelpers {
+  test("static (+)") {
+    assertStaticPlan("(+)",
+      ast.IntegerLiteral(0)
+    )
+
+    assertStaticPlan("(+ 56)",
+      ast.IntegerLiteral(56)
+    )
+
+    assertStaticPlan("(+ (+ 66) (+ -10))",
+      ast.IntegerLiteral(56)
+    )
+
+    assertStaticPlan("(+ 56 -6)",
+      ast.IntegerLiteral(50)
+    )
+
+    assertStaticPlan("(+ 56 (+ -3 -3))",
+      ast.IntegerLiteral(50)
+    )
+
+    assertStaticPlan("(+ 56.5 -6.5)",
+      ast.FlonumLiteral(50.0)
+    )
+
+    assertStaticPlan("(+ 100.0 200.0)",
+      ast.FlonumLiteral(300.0)
+    )
+  }
+
+  test("static (-)") {
+    assertStaticPlan("(- 5)",
+      ast.IntegerLiteral(-5)
+    )
+
+    assertStaticPlan("(- 200 50)",
+      ast.IntegerLiteral(150)
+    )
+
+    assertStaticPlan("(- 200 50.0)",
+      ast.FlonumLiteral(150.0)
+    )
+  }
+
+  test("static (*)") {
+    assertStaticPlan("(*)",
+      ast.IntegerLiteral(1)
+    )
+
+    assertStaticPlan("(* 56)",
+      ast.IntegerLiteral(56)
+    )
+
+    assertStaticPlan("(* (* 7) (* 8))",
+      ast.IntegerLiteral(56)
+    )
+
+    assertStaticPlan("(* 5 -6)",
+      ast.IntegerLiteral(-30)
+    )
+
+    assertStaticPlan("(* 0.5 -6)",
+      ast.FlonumLiteral(-3.0)
+    )
+  }
+
+  test("static (/)") {
+    assertStaticPlan("(/ 0.25)",
+      ast.FlonumLiteral(4.0)
+    )
+
+    assertStaticPlan("(/ 10 2)",
+      ast.IntegerLiteral(5)
+    )
+
+    assertStaticPlan("(/ 0.5 -0.125)",
+      ast.FlonumLiteral(-4.0)
+    )
+
+    assertStaticPlan("(/ 200.0 20.0 5.0 0.25)",
+      ast.FlonumLiteral(8.0)
+    )
+  }
+
+  test("static (truncate-quotient)") {
+    assertStaticPlan("(truncate-quotient 5 2)",
+      ast.IntegerLiteral(2)
+    )
+
+    assertStaticPlan("(truncate-quotient -5 2)",
+      ast.IntegerLiteral(-2)
+    )
+
+    assertStaticPlan("(truncate-quotient 5 -2)",
+      ast.IntegerLiteral(-2)
+    )
+
+    assertStaticPlan("(truncate-quotient -5 -2)",
+      ast.IntegerLiteral(2)
+    )
+
+    intercept[DivideByZeroException] {
+      planStepsFor("(truncate-quotient 5 0)")
+    }
+  }
+
+  test("static (truncate-remainder)") {
+    assertStaticPlan("(truncate-remainder 5 2)",
+      ast.IntegerLiteral(1)
+    )
+
+    assertStaticPlan("(truncate-remainder -5 2)",
+      ast.IntegerLiteral(-1)
+    )
+
+    assertStaticPlan("(truncate-remainder 5 -2)",
+      ast.IntegerLiteral(1)
+    )
+
+    assertStaticPlan("(truncate-remainder -5 -2)",
+      ast.IntegerLiteral(-1)
+    )
+
+    intercept[DivideByZeroException] {
+      planStepsFor("(truncate-remainder 5 0)")
+    }
+  }
+}
--- compiler/src/test/scala/planner/reportproc/NumberProcSuite.scala
+++ compiler/src/test/scala/planner/reportproc/NumberProcSuite.scala
@@ -1,390 +1,261 @@
 package io.llambda.compiler.planner.reportproc
 import io.llambda
 
-import llambda.compiler.{DivideByZeroException}
 import llambda.compiler.ast
 import llambda.compiler.planner.PlanHelpers
 import org.scalatest.FunSuite
 
 class NumberProcSuite extends FunSuite with PlanHelpers {
-  test("static (+)") {
-    assertStaticPlan("(+)",
-      ast.IntegerLiteral(0)
-    )
-    
-    assertStaticPlan("(+ 56)",
-      ast.IntegerLiteral(56)
-    )
-    
-    assertStaticPlan("(+ (+ 66) (+ -10))",
-      ast.IntegerLiteral(56)
-    )
-    
-    assertStaticPlan("(+ 56 -6)",
-      ast.IntegerLiteral(50)
-    )
-    
-    assertStaticPlan("(+ 56 (+ -3 -3))",
-      ast.IntegerLiteral(50)
-    )
-    
-    assertStaticPlan("(+ 56.5 -6.5)",
-      ast.FlonumLiteral(50.0)
-    )
-    
-    assertStaticPlan("(+ 100.0 200.0)",
-      ast.FlonumLiteral(300.0)
-    )
-  }
-  
-  test("static (-)") {
-    assertStaticPlan("(- 5)",
-      ast.IntegerLiteral(-5)
-    )
-    
-    assertStaticPlan("(- 200 50)",
-      ast.IntegerLiteral(150)
-    )
-    
-    assertStaticPlan("(- 200 50.0)",
-      ast.FlonumLiteral(150.0)
-    )
-  }
-  
-  test("static (*)") {
-    assertStaticPlan("(*)",
-      ast.IntegerLiteral(1)
-    )
-    
-    assertStaticPlan("(* 56)",
-      ast.IntegerLiteral(56)
-    )
-    
-    assertStaticPlan("(* (* 7) (* 8))",
-      ast.IntegerLiteral(56)
-    )
-    
-    assertStaticPlan("(* 5 -6)",
-      ast.IntegerLiteral(-30)
-    )
-    
-    assertStaticPlan("(* 0.5 -6)",
-      ast.FlonumLiteral(-3.0)
-    )
-  }
-  
-  test("static (/)") {
-    assertStaticPlan("(/ 0.25)",
-      ast.FlonumLiteral(4.0)
-    )
-
-    assertStaticPlan("(/ 10 2)",
-      ast.IntegerLiteral(5)
-    )
-
-    assertStaticPlan("(/ 0.5 -0.125)",
-      ast.FlonumLiteral(-4.0)
-    )
-
-    assertStaticPlan("(/ 200.0 20.0 5.0 0.25)",
-      ast.FlonumLiteral(8.0)
-    )
-  }
-
   test("static (=)") {
     assertStaticPlan("(= 50 50)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(= 50 200)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(= 200 50)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(= 50.0 50.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(= 50.0 200.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(= 200.0 50.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(= 50 50.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(= 50.0 50)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(= 50.0 50 50.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(= 50.0 50 50)",
       ast.BooleanLiteral(true)
     )
   }
-  
+
   test("static (<)") {
     assertStaticPlan("(< 50 50)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(< 50 200)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(< 200 50)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(< 50.0 50.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(< 50.0 200.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(< 200.0 50.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(< 50.0 200.0 500)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(< 200.0 50.0 10)",
       ast.BooleanLiteral(false)
     )
   }
-  
+
   test("static (<=)") {
     assertStaticPlan("(<= 50 50)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(<= 50 200)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(<= 200 50)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(<= 50.0 50.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(<= 50.0 200.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(<= 200.0 50.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(<= 50.0 200.0 200)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(<= 200.0 50.0 -100)",
       ast.BooleanLiteral(false)
     )
   }
-  
+
   test("static (>)") {
     assertStaticPlan("(> 50 50)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(> 50 200)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(> 200 50)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(> 50.0 50.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(> 50.0 200.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(> 200.0 50.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(> 200.0 50.0 0.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(> 200.0 50.0 200.0)",
       ast.BooleanLiteral(false)
     )
   }
-  
+
   test("static (>=)") {
     assertStaticPlan("(>= 50 50)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(>= 50 200)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(>= 200 50)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(>= 50.0 50.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(>= 50.0 200.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(>= 200.0 50.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(>= 200.0 50.0 0.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(>= 200.0 50.0 200.0)",
       ast.BooleanLiteral(false)
     )
   }
-  
+
   test("static (exact)") {
     assertStaticPlan("(exact 50)",
       ast.IntegerLiteral(50)
     )
-    
+
     assertStaticPlan("(exact -60.0)",
       ast.IntegerLiteral(-60)
     )
   }
-  
+
   test("static (inexact)") {
     assertStaticPlan("(inexact 50)",
       ast.FlonumLiteral(50.0)
     )
-    
+
     assertStaticPlan("(inexact -60.0)",
       ast.FlonumLiteral(-60.0)
     )
   }
-  
+
   test("static (positive?)") {
     assertStaticPlan("(positive? 50)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(positive? 0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(positive? -200.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(positive? +nan.0)",
       ast.BooleanLiteral(false)
     )
   }
-  
+
   test("static (negative?)") {
     assertStaticPlan("(negative? 50)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(negative? 0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(negative? -200.0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(negative? +nan.0)",
       ast.BooleanLiteral(false)
     )
   }
-  
+
   test("static (zero?)") {
     assertStaticPlan("(zero? 50)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(zero? 0)",
       ast.BooleanLiteral(true)
     )
-    
+
     assertStaticPlan("(zero? -200.0)",
       ast.BooleanLiteral(false)
     )
-    
+
     assertStaticPlan("(zero? +nan.0)",
       ast.BooleanLiteral(false)
     )
-  }
-
-  test("static (truncate-quotient)") {
-    assertStaticPlan("(truncate-quotient 5 2)",
-      ast.IntegerLiteral(2)
-    )
-
-    assertStaticPlan("(truncate-quotient -5 2)",
-      ast.IntegerLiteral(-2)
-    )
-
-    assertStaticPlan("(truncate-quotient 5 -2)",
-      ast.IntegerLiteral(-2)
-    )
-
-    assertStaticPlan("(truncate-quotient -5 -2)",
-      ast.IntegerLiteral(2)
-    )
-
-    intercept[DivideByZeroException] {
-      planStepsFor("(truncate-quotient 5 0)")
-    }
-  }
-
-  test("static (truncate-remainder)") {
-    assertStaticPlan("(truncate-remainder 5 2)",
-      ast.IntegerLiteral(1)
-    )
-
-    assertStaticPlan("(truncate-remainder -5 2)",
-      ast.IntegerLiteral(-1)
-    )
-
-    assertStaticPlan("(truncate-remainder 5 -2)",
-      ast.IntegerLiteral(1)
-    )
-
-    assertStaticPlan("(truncate-remainder -5 -2)",
-      ast.IntegerLiteral(-1)
-    )
-
-    intercept[DivideByZeroException] {
-      planStepsFor("(truncate-remainder 5 0)")
-    }
   }
 
   test("static (min)") {
