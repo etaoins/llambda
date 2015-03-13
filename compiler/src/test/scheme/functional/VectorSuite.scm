4d9b284d431b3a3ec53585770bffdb615afc3cd6
Ryan Cumming <etaoins@gmail.com>
2015-03-13 21:18:26 +1100

    compiler: Merge some vector functional tests
    Many of these predate our test assertion framework

--- compiler/src/test/scheme/functional/VectorSuite.scm
+++ compiler/src/test/scheme/functional/VectorSuite.scm
@@ -8,32 +8,22 @@
   (assert-true  (vector? (typeless-cell #())))
   (assert-false (vector? (typeless-cell 4)))))
 
-(define-test "(make-vector) an uninitialized empty vector" (expect #()
-  (make-vector 0)))
-
-(define-test "(make-vector) an filled empty vector" (expect #()
-  (make-vector 0 5.0)))
-
-(define-test "(make-vector) an unintialized non-empty vector" (expect #(#!unit #!unit #!unit)
-  (make-vector 3)))
-
-(define-test "(make-vector) a filled non-empty vector" (expect #(5.0 5.0 5.0)
-  (make-vector 3 5.0)))
+(define-test "(make-vector)" (expect-success
+  (assert-equal #() (make-vector 0))
+  (assert-equal #() (make-vector 0 5.0))
+  (assert-equal #(#!unit #!unit #!unit) (make-vector 3))
+  (assert-equal #(5.0 5.0 5.0) (make-vector 3 5.0))))
 
 (define-test "(make-vector) with a negative length fails" (expect-error range-error?
   (make-vector -3)))
 
-(define-test "(vector) an empty vector" (expect #()
-  (vector)))
+(define-test "(vector) " (expect-success
+  (assert-equal #() (vector))
+  (assert-equal #(a b c) (vector 'a 'b 'c))))
 
-(define-test "(vector) a non-empty vector" (expect #(a b c)
-  (vector 'a 'b 'c)))
-
-(define-test "(list->vector) an empty vector" (expect #()
-  (list->vector '())))
-
-(define-test "(list->vector) a non-empty vector" (expect #(a b c)
-  (list->vector '(a b c))))
+(define-test "(list->vector) an empty vector" (expect-success
+  (assert-equal #() (list->vector '()))
+  (assert-equal #(a b c) (list->vector '(a b c)))))
 
 (define-test "(list->vector) with non-list fails" (expect-error type-error?
   (list->vector 'a)))
@@ -42,11 +32,9 @@
   (assert-equal 3 (vector-length #(1 2 3)))
   (assert-equal 0 (vector-length #()))))
 
-(define-test "vector length of non-empty constructed vector" (expect 15
-  (vector-length (make-vector 15 #f))))
-
-(define-test "vector length of empty constructed vector" (expect 0
-  (vector-length (make-vector 0 #f))))
+(define-test "dynamic (vector-length)" (expect-success
+  (assert-equal 0 (vector-length (make-vector 0 #f)))
+  (assert-equal 15 (vector-length (make-vector 15 #f)))))
 
 (define-test "static (vector-ref)" (expect-static-success
   (assert-equal 'c (vector-ref #(a b c d e f) 2))))
@@ -81,17 +69,11 @@
   (define test-vector (make-vector 5 1))
   (vector-set! test-vector -1 2)))
 
-(define-test "(vector-append) with no arguments" (expect #()
-  (vector-append)))
-
-(define-test "(vector-append) with single argument" (expect #(a b c)
-  (vector-append #(a b c))))
-
-(define-test "(vector-append) three vectors" (expect #(1 2 3 4 5 6)
-  (vector-append #(1 2) #(3 4) #(5 6))))
-
-(define-test "(vector-append) three empty vectors" (expect #()
-  (vector-append #() #() #())))
+(define-test "(vector-append)" (expect-success
+  (assert-equal #() (vector-append))
+  (assert-equal #(a b c) (vector-append #(a b c)))
+  (assert-equal #(1 2 3 4 5 6) (vector-append #(1 2) #(3 4) #(5 6)))
+  (assert-equal #() (vector-append #() #() #()))))
 
 (define-test "(vector-append) with non-vector fails" (expect-error type-error?
   (vector-append '(1 2) '(3 4))))
