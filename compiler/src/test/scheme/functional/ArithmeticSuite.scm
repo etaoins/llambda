e0a7a855617eefdddcd57c671be30593e73a94bd
Ryan Cumming <etaoins@gmail.com>
2016-12-10 18:50:49 +1100

    compiler: Test generating native float math
    There were no tests for generating floating point arithmetic
    instructions.

--- compiler/src/test/scheme/functional/ArithmeticSuite.scm
+++ compiler/src/test/scheme/functional/ArithmeticSuite.scm
@@ -10,8 +10,8 @@
   (assert-within 9223372036854775807 32.0 (+ 9223372036854775807 9223372036854775807 -9223372036854775807.0))))
 
 (define-test "dynamic (+)" (expect-success
-  (define dynamic-5 (length (typeless-cell '(1 2 3 4 5))))
-  (assert-equal 8 (+ dynamic-5 1 2))))
+  (assert-equal 8 (+ (typed-dynamic 5 <integer>) 1 2))
+  (assert-equal 8.0 (+ (typed-dynamic 5.0 <flonum>) 1.0 2.0))))
 
 (define-test "adding single string fails" (expect-error type-error?
   (+ "Hello!")))
@@ -37,8 +37,8 @@
   (assert-within 9223372036854775807 32.0 (* 9223372036854775807 2 0.5))))
 
 (define-test "dynamic (*)" (expect-success
-  (define dynamic-5 (length (typeless-cell '(1 2 3 4 5))))
-  (assert-equal 10 (* dynamic-5 1 2))))
+  (assert-equal 10 (* (typed-dynamic 5 <integer>) 1 2))
+  (assert-equal 10.0 (* (typed-dynamic 5.0 <flonum>) 1.0 2.0))))
 
 (define-test "multiplying single string fails" (expect-error type-error?
   (* "Hello!")))
@@ -63,9 +63,10 @@
   (assert-within 9 32.0 (- -9223372036854775807 9223372036854775807 -9223372036854775807 -9223372036854775807.0))))
 
 (define-test "dynamic (-)" (expect-success
-  (define dynamic-5 (length (typeless-cell '(1 2 3 4 5))))
-  (assert-equal 2 (- dynamic-5 1 2))
-  (assert-equal -6 (- 1 2 dynamic-5))))
+  (assert-equal 2 (- (typed-dynamic 5 <integer>) 1 2))
+  (assert-equal -6 (- 1 2 (typed-dynamic 5 <integer>)))
+  (assert-equal 2.0 (- (typed-dynamic 5.0 <flonum>) 1.0 2.0))
+  (assert-equal -6.0 (- 1.0 2.0 (typed-dynamic 5.0 <flonum>)))))
 
 (define-test "subtracting no numbers fails" (expect-error arity-error?
   (-)))
@@ -115,6 +116,7 @@
   (assert-equal 9223372036854775808.0 (/ -9223372036854775808 -1))))
 
 (define-test "dynamic (/)" (expect-success
+  (assert-equal 2.0 (/ 20.0 (typed-dynamic 5.0 <flonum>) 2.0))
   (assert-equal 9223372036854775808.0 (/ (typed-dynamic -9223372036854775808 <integer>) -1))
   (assert-equal 9223372036854775808.0 (/ -9223372036854775808 (typed-dynamic -1 <integer>)))))
 
