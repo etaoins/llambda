3287b5e8c46f28c19dbd4b55d9f4cb61ed90dabe
Ryan Cumming <etaoins@gmail.com>
2016-12-21 20:02:35 +1100

    compiler: ValueConversionSuite typo fixes

--- compiler/src/test/scheme/functional/ValueConversionSuite.scm
+++ compiler/src/test/scheme/functional/ValueConversionSuite.scm
@@ -1,16 +1,16 @@
-(define-test "datum can be cast to pair" (expect 1
-  ; This assumes (vector-ref) takes a pair
+(define-test "datum can be cast to vector" (expect 1
+  ; This assumes (vector-ref) takes a vector
   (vector-ref (typeless-cell #(1)) 0)))
 
 (define-test "#false can be unboxed as truthy" (expect #t
-  ; This assumes (not) takes a truthy boolean
+  ; This assumes (not) takes a truthy
   (not (dynamic-false))))
 
 (define-test "#false can be unboxed as bool" (expect #t
   ; This assumes (boolean=?) takes two native booleans
   (boolean=? #f (dynamic-false))))
 
-(define-test "#true can be unboxed as boolean" (expect #f
+(define-test "#true can be unboxed as truthy" (expect #f
   ; This assumes (not) takes a truthy
   (not (typeless-cell #t))))
 
@@ -27,11 +27,11 @@
   (boolean=? #t (typeless-cell '()))))
 
 (define-test "integer can be unboxed as native integer" (expect #(#t #t #t)
-  ; This assumes (make-vector) takes an native integer
+  ; This assumes (make-vector) takes a native integer length
   (make-vector (typeless-cell 3) #t)))
 
 (define-test "flonum cannot be unboxed as native integer" (expect-error type-error?
-  ; This assumes (make-vector) takes an native integer
+  ; This assumes (make-vector) takes a native integer length
   (make-vector (typeless-cell 3.0) #t)))
 
 (define-test "flonum can be unboxed as double" (expect 1.0
@@ -75,7 +75,7 @@
   (define flonum->double (world-function system-library "llbase_test" (-> <flonum> <native-double>)))
   (flonum->double -53)))
 
-(define-test "'3' can be unboxed as a character" (expect 3
+(define-test "#\\3 can be unboxed as a character" (expect 3
   (import (scheme char))
   ; This assumes (digit-value) takes an native Unicode character
   (digit-value (typeless-cell #\3))))
@@ -93,7 +93,7 @@
   ; Thie assumes (boolean=? takes two bools)
   (boolean=? (not #t) (not #f))))
 
-(define-test "integer can be passed to a procedure as float" (expect-error type-error?
+(define-test "integer cannot be passed to a procedure as float" (expect-error type-error?
   (import (llambda nfi))
 
   ; Nothing in the stdlib takes float
@@ -103,7 +103,7 @@
 ; Make sure if we use type analysis to short circuit bool evaluation do it right
 ; This was also broken at one point
 (define-test "types that cannot be boolean evaluate as true" (expect #f
-  ; This assumes (not) takes an native boolean and (con) returns a pair
+  ; This assumes (not) takes an native boolean and (cons) returns a pair
   (not (cons 1 2))))
 
 ; This was broken due to list element being an abstract type
@@ -135,17 +135,17 @@
   (native-make-bytevector 1 256)))
 
 (define-test "converting an integer too large for <native-uint32> fails" (expect-error type-error?
-  ; This assumes (make-list) takes a <native-uint32>
+  ; This assumes (make-list) takes a <native-uint32> length
   (make-list (typeless-cell (+ (expt 2 32) 1)))))
 
 (define-test "converting an integer too small for <native-int32> fails" (expect-error type-error?
-  ; This assumes (make-list) takes a <native-uint32>
+  ; This assumes (make-list) takes a <native-uint32> length
   (make-list (typeless-cell -1))))
 
 (define-test "converting an integer too large for <native-uint8> fails" (expect-error type-error?
-  ; This assumes (make-list) takes a <native-uint8>
+  ; This assumes (make-bytevector) takes a <native-uint8> fill
   (make-bytevector 1 (typeless-cell 256))))
 
 (define-test "converting an integer too small for <native-int8> fails" (expect-error type-error?
-  ; This assumes (make-list) takes a <native-uint8>
+  ; This assumes (make-bytevector) takes a <native-uint8> fill
   (make-bytevector 1 (typeless-cell -1))))
