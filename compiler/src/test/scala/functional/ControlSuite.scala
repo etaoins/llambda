a5db6b80930606bb607e79916ff79bb487d6126b
Ryan Cumming <etaoins@gmail.com>
2017-04-28 19:43:16 +1000

    compiler: Unswap LazySuite and ControlSuite
    These files somehow contained code for each other's classes

--- compiler/src/test/scala/functional/ControlSuite.scala
+++ compiler/src/test/scala/functional/ControlSuite.scala
@@ -1,4 +1,4 @@
 package io.llambda.compiler.functional
 import io.llambda
 
-class LazySuite extends SchemeFunctionalTestRunner("LazySuite")
+class ControlSuite extends SchemeFunctionalTestRunner("ControlSuite")
