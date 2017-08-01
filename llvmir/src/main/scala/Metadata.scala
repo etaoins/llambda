126c0d781bad0d3e86601dfdd87aecb0e501130c
Ryan Cumming <etaoins@gmail.com>
2017-08-02 07:42:09 +1000

    llvmir: Remove unused method

--- llvmir/src/main/scala/Metadata.scala
+++ llvmir/src/main/scala/Metadata.scala
@@ -35,19 +35,6 @@ abstract class MetadataNode extends Metadata {
   def toIr: String = {
     "!{" + operandOpts.map(operandOptToIr).mkString(", ") + "}"
   }
-
-  protected def listToNotNullMetadata(metadataList: List[Metadata]): Metadata = {
-    if (metadataList.isEmpty) {
-      // Not sure why this is a thing
-      UserDefinedMetadataNode(List(
-        Some(IntegerConstant(IntegerType(32), 0))
-      ))
-    }
-    else {
-      UserDefinedMetadataNode(metadataList.map(Some(_)))
-    }
-  }
-
 }
 
 case class UserDefinedMetadataNode(operandOpts: Seq[Option[MetadataOperand]]) extends MetadataNode
